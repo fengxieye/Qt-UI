8324f926d333e3602a453589cbb542b6eb3439cd
zhongjianhui <jianhui.zhong@timelink.cn>
2019-03-22 12:03:47 +0800

    给label的鼠标左点击加个信号

---
+++ ClickableLabel/ClickableLabel.cpp
@@ -0,0 +1,19 @@
+#include "ClickableLabel.h"
+
+ClickableLabel::ClickableLabel(QWidget *parent) : QLabel(parent)
+{
+
+}
+
+ClickableLabel::~ClickableLabel()
+{
+
+}
+
+void ClickableLabel::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        emit clicked();
+    }
+}
